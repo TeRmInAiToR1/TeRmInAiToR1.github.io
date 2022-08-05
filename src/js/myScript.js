ae405093d03aca76fc4966f3e5c69525894344c0
TeRmInAiToR1 <93791095+TeRmInAiToR1@users.noreply.github.com>
2022-08-05 22:43:13 +0400

    работа с jquery

--- src/js/myScript.js
+++ src/js/myScript.js
@@ -1,45 +1,183 @@
 "use strict"
 
-alert("Рад приветствовать тебя на моем сайте!\nДавай произведем расчет стоимости моих услуг");
+$(window).on('load', function () {
+    $('.preloader').fadeOut('slow');
+//    $('.preloader').delay(2500).fadeOut('slow');
+});
 
-let design = prompt("Начнем с дизайна\nКакой дизайн вы предпочитаете?\n1 - Hi Tech\n2 - Organic & Natural\n3 - Полигональный");
-let type = prompt("Выберите тип сайта\n1 - Социальные\n2 - Комерческие\n3 - Информационные");
-let adaptability = prompt("Адаптивность\n1 - Резиновый\n2 - Pixel Perfect\n3 - Адаптивный");
+$(document).ready(function(){
+    $(window).scroll(function(){
+        let scrollDistance = $(window).scrollTop();
+        $(".section").each(function(i, el){
+            if($(el).offset().top - $("nav").outerHeight() - 100 <= scrollDistance){
+                $("nav a").each(function(i, el){
+                    if($(el).hasClass("activeNav")){
+                        $(el).removeClass("activeNav");
+                    }
+                });
+                
+                $("nav .menu li a:eq("+ i +")").addClass("activeNav");
+            }
+            if (scrollDistance < 600){
+                $("nav a").each(function(i, el){
+                    if($(el).hasClass("activeNav")){
+                        $(el).removeClass("activeNav");
+                    }
+                });
+            } 
+        });
+    });
+    
+    let options = {threshold: [0.5]};//анимация сработает при появлении объекта на 50%
+    let observer = new IntersectionObserver(onEntry, options);
+    let elements = $(".element-animation");
+    elements.each(function(i, el){
+        observer.observe(el);
+    });
+    
+    observer = new IntersectionObserver(onEntryST, options);
+    elements = $(".element-animation-sk");
+    elements.each(function(i, el){
+        observer.observe(el);
+    });
+    
+    let imgObserver = new IntersectionObserver(onEntryImg, options);
+    let imgElements = $(".imgUploads1");
+    imgElements.each(function(i, el){
+        imgObserver.observe(el);
+    });
+    
+    options = {threshold: [0.2]};
+    imgElements = $(".imgUploads2");
+    imgObserver = new IntersectionObserver(onEntryIm, options);
+    imgElements.each(function(i, el){
+        imgObserver.observe(el);
+    });
+    
+    let optionsCarousel = {threshold: [0.1]};//анимация сработает при появлении объекта на 50%
+    let observerCarousel = new IntersectionObserver(onEntryCarousel, optionsCarousel);
+    let elementsCarousel = $(".image-link");
+    elementsCarousel.each(function(i, el){
+        observerCarousel.observe(el);
+    });
+    
+    //setTimeout(function(){ $(".my_modal").trigger('click'); }, 10000);
+    
+    $('.image-link').magnificPopup({type:'image'});
+    
+    let show = true;
+    let countbox = ".benefits__inner";
+    $(window).on("scroll load resize", function () {
+        if (!show) return false; // Отменяем показ анимации, если она уже была выполнена
+        let w_top = $(window).scrollTop(); // Количество пикселей на которое была прокручена страница
+        let e_top = $(countbox).offset().top; // Расстояние от блока со счетчиками до верха всего документа
+        let w_height = $(window).height(); // Высота окна браузера
+        let d_height = $(document).height(); // Высота всего документа
+        let e_height = $(countbox).outerHeight(); // Полная высота блока со счетчиками
+        if (w_top + 500 >= e_top || w_height + w_top == d_height || e_height + e_top < w_height) {
+            $('.number').css('opacity', '1');
+            $('.number').spincrement({
+                thousandSeparator: "",
+                duration: 3000,
+            });
+             
+            show = false;
+        }
+    });
+});
+    
+function countPrice(){
+    let design = $('#siteDesign option:selected').text();
+    let type = $('#siteType option:selected').text();
+    let adaptability = $('#siteAdapt option:selected').text();
+    
+    let answers = {
+        design: design,
+        type: type,
+        adaptability: adaptability,
+        cost: 0,
+        terms: 0,
+        calculations(){
+            if(this.design == 'Hi Tech'){
+                this.cost += 2000;
+                this.terms += 2;
+            }
+            else if(this.design == 'Organic & Natural'){
+                this.cost += 1500;
+                this.terms += 2;
+            }
+            else if(this.design == 'Полигональный'){
+                this.cost += 1000;
+                this.terms += 1;
+            }
 
-let answers = {
-    design: design,
-    type: type,
-    adaptability: adaptability,
-    cost: 0,
-    terms: 0,
-    calculations(){
-        
-        if(this.design == 1){
-            this.cost += 2000;
-            this.terms += 2;
+            if(this.type == 'Социальный') { this.cost += 3000; this.terms += 4; }
+            else if(this.type == 'Комерческий') { this.cost += 4000; this.terms += 5; }
+            else if(this.type == 'Информационный') { this.cost += 1500; this.terms += 1; }
+
+            if(this.adaptability == 'Резиновый') { this.cost += 1500; this.terms += 2; }
+            else if(this.adaptability == 'Pixel Perfect') { this.cost += 2000; this.terms += 1; }
+            else if(this.adaptability == 'Адаптивный') { this.cost += 5000; this.terms += 4; }
         }
-        else if(this.design == 2){
-            this.cost += 1500;
-            this.terms += 2;
-        }
-        else if(this.design == 3){
-            this.cost += 1000;
-            this.terms += 1;
-        }
-        
-        if(this.type == 1) { this.cost += 3000; this.terms += 4; }
-        else if(this.type == 2) { this.cost += 4000; this.terms += 5; }
-        else if(this.type == 3) { this.cost += 1500; this.terms += 1; }
-        
-        if(this.adaptability == 1) { this.cost += 1500; this.terms += 2; }
-        else if(this.adaptability == 2) { this.cost += 2000; this.terms += 1; }
-        else if(this.adaptability == 3) { this.cost += 5000; this.terms += 4; }
-    },
-    showPriceAndTerms(){
-        alert("Цена вашего будушего сайта >> " + answers.cost + " рублей\nСроки его выполнения >> " + answers.terms + " дней");
-    }
-};
-//console.log(answers);
-console.log(answers.cost);
-answers.calculations();
-answers.showPriceAndTerms();
+    };
+    answers.calculations();
+    console.log(answers.cost);
+    let num_f = $('.numbers:first');
+    let num_s = $('.numbers:last'); 
+    num_f.text(answers.terms + " дней");
+    num_s.text(answers.cost + " рублей");
+    
+    num_f.removeClass('col-1');
+    num_f.addClass('col-4');
+    
+    num_s.removeClass('col-1');
+    num_s.addClass('col-4');
+}
+
+function onEntry(entry){
+    entry.forEach(change => {
+       if(change.isIntersecting){
+           change.target.classList.add('show-animation');
+       } 
+    });
+}
+function onEntryST(entry){
+    entry.forEach(change => {
+       if(change.isIntersecting){
+           change.target.classList.add('show-animation-sk');
+       } 
+    });
+}
+
+function onEntryImg(entry){
+    entry.forEach(change => {
+       if(change.isIntersecting){
+           $('.about_me img').each( function(){ this.src = this.getAttribute('data_src');
+                                              });
+       } 
+    });
+}
+function onEntryIm(entry){
+    entry.forEach(change => {
+       if(change.isIntersecting){
+           $('.reviews_container img').each( function(){ this.src = this.getAttribute('data_src');});
+       } 
+    });
+}
+function onEntryCarousel(entry){
+    entry.forEach(change => {
+       if(change.isIntersecting){
+           $('.carousel_inner img').each( function(){ this.src = this.getAttribute('data_src');
+                                              });
+       } 
+    });
+}
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+$('a[href^="#"]').click(function(){//плавный скрол к секциям
+    let valHref = $(this).attr("href");
+    $('html, body').animate({scrollTop: $(valHref).offset().top - 50 + "px"});
+});
